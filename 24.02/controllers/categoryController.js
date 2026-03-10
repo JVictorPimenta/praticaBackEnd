3947c00311a781307afa06f61767b4fd282ad7b3
João Victor Pimenta R <joaovictorp.r77@gmail.com>
2026-03-10 10:37:16 -0300

    slide 10

--- 24.02/controllers/categoryController.js
+++ 24.02/controllers/categoryController.js
@@ -36,6 +36,31 @@ const categoryController = {
       return res.status(500).json({ error: error.message });
     }
   },
+  getCategoryProducts: async (req, res) => {
+    try {
+      const {id} = req.params;
+      const category = await Category.findByPk(id, { include:'products' });
+      if (!category) {
+        return res.status(404).json({
+          success: false,
+          data: null,
+          message: 'categoria não enontrada'
+        });
+      }
+      res.status(200).json({
+        succes: true,
+        data: category.products,
+        message: 'produtos da categoria recuperados com sucesso'
+      });
+    } catch (error) {
+      res.status(500).json({
+        sucess: false,
+        data: null,
+        message: 'falha ao recuperar produtos da categoria'
+      });
+      console.log(error);
+    }
+  },
   create: async (req, res) => {
     try {
       const { name, description } = req.body;
