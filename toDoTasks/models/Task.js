4dbcfffb7806a01bf8dcafeede08600872b002f7
João Victor Pimenta R <joaovictorp.r77@gmail.com>
2026-03-11 10:16:38 -0300

    basicamente terminada

--- toDoTasks/models/Task.js
+++ toDoTasks/models/Task.js
@@ -2,6 +2,11 @@ import { DataTypes } from "sequelize";
 import {sequelize} from "../database/sqlConnection.js";
 
 const Task = sequelize.define('Task', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     title: {
         type: DataTypes.STRING,
         allowNull: false
